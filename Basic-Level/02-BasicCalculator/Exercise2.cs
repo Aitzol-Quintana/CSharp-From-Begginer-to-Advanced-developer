a6d909fbb73a12c9c2d08f934573b62c41e573bb
Aitzol-Quintana <aitzolq@gmail.com>
2025-05-31 13:52:47 +0200

    Completed Basic Exercise #2: Basic calculator

---
+++ Basic-Level/02-BasicCalculator/Exercise2.cs
@@ -0,0 +1,41 @@
+﻿
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+public class BasicCalculator
+{
+    static int a,b;
+    static int result;
+    static string error = "";
+    public static void Main()
+    {
+        Console.WriteLine("Introduce your first number");
+        a=Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Introduce your second number");
+        b = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("What do you want to do?");
+
+        switch (Console.ReadLine())
+        {
+            case ("+"):
+                result = a + b;
+                break;
+            case ("-"):
+                result = a - b;
+                break;
+            case ("*"):
+                result = a * b;
+                break;
+            case ("/"):
+                if (b == 0) error = "Error: Division by zero";
+                else result = a / b;
+                break;
+            default: error = "Invalid operation"; break;
+
+        }
+        if (error != "") Console.WriteLine(error);
+        else Console.WriteLine($"Result: {result}");
+        
+    }
+}
